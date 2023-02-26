d3384d8d085f825f58b8dd1e7931e85fb6053b0c
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-02-27 10:22:32 +1300

    20230227

---
+++ speech-recognition/src/App.jsx
@@ -0,0 +1,26 @@
+import SpeechRecognition, {useSpeechRecognition} from "react-speech-recognition"
+
+function App() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={()=>SpeechRecognition.startListening({continuous:true})}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
+  );
+}
+
+export default App
